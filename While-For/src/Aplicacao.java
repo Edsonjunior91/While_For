80b2a17bf8a82819ba344bdd85b897db3ab5e71d
Edson Junior <edson_junior91@hotmail.com>
2020-06-30 00:50:08 -0300

    While e For

---
+++ While-For/src/Aplicacao.java
@@ -0,0 +1,53 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		int x = 0;
+		
+	//	while (x <= 10) {
+	//		System.out.println(x);
+	//		x++;
+	//	}
+		
+	//	while(true) {
+	//		System.out.println(x);
+	//		
+	//		if (x == 10) {
+	//			break;
+	//		}
+	//		
+	//		x++;
+	//	}
+		
+	//	while( x <= 10) {
+	//		x++;
+	//		if (x == 5) {
+	//			continue;
+	//		}
+	//	}
+		
+	//	do {
+	//		System.out.println(x);
+	//		x++;
+	//	}while (x <=10);
+		
+	//	int i;
+	//	int j;
+	//	for(i = 0, j = 10; i < 10; i++, j--) {
+	//		System.out.println(i + " - " + j);
+	//	}
+		
+	//	for(int i = 0; i < 10; i++) {
+	//		
+	//	}
+		int i = 0;
+		for(; ;) {
+			System.out.println(i);
+			i++;
+			if ( i == 10) {
+				break;
+			}
+		}
+	}
+
+}
